83ce84660a62423ea6bd5af61bde55b7f47c5016
tidusjar <jamierees@outlook.com>
2016-02-29 11:40:41 +0000

    added helpers

---
+++ RequestPlex.Store/ISettingsRepository.cs
@@ -0,0 +1,70 @@
+﻿#region Copyright
+// /************************************************************************
+//    Copyright (c) 2016 Jamie Rees
+//    File: ISettingsRepository.cs
+//    Created By: Jamie Rees
+//   
+//    Permission is hereby granted, free of charge, to any person obtaining
+//    a copy of this software and associated documentation files (the
+//    "Software"), to deal in the Software without restriction, including
+//    without limitation the rights to use, copy, modify, merge, publish,
+//    distribute, sublicense, and/or sell copies of the Software, and to
+//    permit persons to whom the Software is furnished to do so, subject to
+//    the following conditions:
+//   
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+//   
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  ************************************************************************/
+#endregion
+using System.Collections.Generic;
+
+using RequestPlex.Store.Models;
+
+namespace RequestPlex.Store
+{
+    public interface ISettingsRepository
+    {
+        /// <summary>
+        /// Inserts the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        long Insert(GlobalSettings entity);
+
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<GlobalSettings> GetAll();
+
+        /// <summary>
+        /// Gets the specified identifier.
+        /// </summary>
+        /// <param name="settingsName">Name of the settings.</param>
+        /// <returns></returns>
+        GlobalSettings Get(string settingsName);
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        bool Delete(GlobalSettings entity);
+
+        /// <summary>
+        /// Updates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        bool Update(GlobalSettings entity);
+
+
+    }
+}
