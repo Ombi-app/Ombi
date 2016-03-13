cc3d41aa9a3d5687e59e6fbb0fd89292ad45f054
Jamie Rees <jamie.rees@exclaimer.com>
2016-03-13 21:32:47 +0000

    Small changes

---
+++ PlexRequests.Core/SettingModels/EmailNotificationSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace PlexRequests.Core.SettingModels
+{
+    public class EmailNotificationSettings : Settings
+    {
+        public string EmailHost { get; set; }
+        public int EmailPort { get; set; }
+        public bool EmailAuthentication { get; set; }
+        public string RecipientEmail { get; set; }
+        public string EmailUsername { get; set; }
+        public string EmailPassword { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
