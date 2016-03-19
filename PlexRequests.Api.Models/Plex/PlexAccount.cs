c28a26413aa01e0a18f66fc394caa035587f1ec6
Shannon Barrett <shannon.barrett@digintent.com>
2016-03-18 19:16:29 -0500

    Should fix issue #36

---
+++ PlexRequests.Api.Models/Plex/PlexAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PlexRequests.Api.Models.Plex
+{
+    [XmlRoot(ElementName = "user")]
+    public class PlexAccount
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "username")]
+        public string Username { get; set; }
+        [XmlAttribute(AttributeName = "email")]
+        public string Email { get; set; }
+        [XmlAttribute(AttributeName = "authenticationToken")]
+        public string AuthToken { get; set; }
+    }
+}
