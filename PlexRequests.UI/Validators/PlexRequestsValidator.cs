c7e3409935f90c6f3c78929d4651e262b0973453
TidusJar <jamierees@outlook.com>
2016-05-12 16:04:25 -0400

    resolved #209

---
+++ PlexRequests.UI/Validators/PlexRequestsValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentValidation;
+using PlexRequests.Core.SettingModels;
+
+namespace PlexRequests.UI
+{
+	public class PlexRequestsValidator : AbstractValidator<PlexRequestSettings>
+	{
+		public PlexRequestsValidator ()
+		{
+			RuleFor (x => x.BaseUrl).NotEqual ("requests").WithMessage ("You cannot use 'requests' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("admin").WithMessage ("You cannot use 'admin' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("search").WithMessage ("You cannot use 'search' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("issues").WithMessage ("You cannot use 'issues' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("userlogin").WithMessage ("You cannot use 'userlogin' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("login").WithMessage ("You cannot use 'login' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("test").WithMessage ("You cannot use 'test' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("approval").WithMessage ("You cannot use 'approval' as this is reserved by the application.");
+			RuleFor (x => x.BaseUrl).NotEqual ("updatechecker").WithMessage ("You cannot use 'updatechecker' as this is reserved by the application.");
+
+		
+		
+		}
+	}
+}
+
