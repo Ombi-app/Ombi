96a40a2aaaaa27cddfd2bc3e393478438fe34eac
TidusJar <jamierees@outlook.com>
2016-05-15 22:16:40 -0400

    Added the updater to the soloution and did a bit of starting code.

---
+++ PlexRequests.Updater/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlexRequests.Updater
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Starting PlexRequests .Net updater");
+
+		}
+	}
+}
---
+++ PlexRequests.Updater/Updater.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using PlexRequests.Core;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+
+namespace PlexRequests.Updater
+{
+	public class Updater
+	{
+		public void Start(){
+			var c = new StatusChecker ();
+
+			try {
+
+				var release = c.GetStatus ();
+				if(!release.UpdateAvailable)
+				{
+					Console.WriteLine ("No Update availble, shutting down");
+				}
+
+				using(var client = new WebClient())
+				using(var ms = new MemoryStream(client.DownloadData(release.DownloadUri), false))
+				using(var gz = new GZipStream(ms, CompressionLevel.Optimal))
+				{
+					// TODO decompress stream
+				}
+
+
+			} catch (Exception ex) {
+				
+				Console.WriteLine (ex.Message);
+				Console.WriteLine ("Oops... Looks like we cannot update!");
+				Console.ReadLine ();
+			}
+		}
+
+		
+		
+	}
+}
+
