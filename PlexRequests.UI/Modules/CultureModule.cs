2ec27dce902dc424c854894e2f5e92b837787611
tidusjar <jamierees@outlook.com>
2016-06-24 10:06:03 +0100

    Implemented the different languages and added the ability to change cultures. #243

---
+++ PlexRequests.UI/Modules/CultureModule.cs
@@ -0,0 +1,63 @@
+﻿#region Copyright
+// /************************************************************************
+//    Copyright (c) 2016 Jamie Rees
+//    File: CultureModule.cs
+//    Created By: Jamie Rees
+//   
+//    Permission is hereby granted, free of charge, to any person obtaining
+//    a copy of this software and associated documentation files (the
+//    "Software"), to deal in the Software without restriction, including
+//    without limitation the rights to use, copy, modify, merge, publish,
+//    distribute, sublicense, and/or sell copies of the Software, and to
+//    permit persons to whom the Software is furnished to do so, subject to
+//    the following conditions:
+//   
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+//   
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  ************************************************************************/
+#endregion
+using System;
+
+using Nancy;
+using Nancy.Extensions;
+using Nancy.Responses;
+
+using PlexRequests.Core;
+using PlexRequests.Core.SettingModels;
+using PlexRequests.UI.Helpers;
+
+namespace PlexRequests.UI.Modules
+{
+    public class CultureModule : BaseModule
+    {
+        private const string CookieName = "_culture";
+        public CultureModule(ISettingsService<PlexRequestSettings> pr) : base("culture",pr)
+        {
+            Get["/culture"] = x => SetCulture();
+        }
+
+        public RedirectResponse SetCulture()
+        {
+            var culture = (string)Request.Query["culture"];
+            var returnUrl = (string)Request.Query["returnUrl"];
+
+            // Validate
+            culture = CultureHelper.GetImplementedCulture(culture);
+
+            var cookie = Request.Cookies["_culture"];
+            var response = Context.GetRedirect(returnUrl);
+
+            response.WithCookie(CookieName, cookie ?? culture, DateTime.Now.AddYears(1));
+
+            return response;
+        }
+    }
+}
