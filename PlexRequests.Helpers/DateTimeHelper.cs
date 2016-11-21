d2b1644e4b9db9c9920a9321c03281c1568408b7
richardpoyner <richardpoynerrasmussen@gmail.com>
2016-11-21 10:35:10 +0100

    Update DateTimeHelper.cs
    I got an 500 error on my NAS.
    This fixed that error.

--- PlexRequests.Helpers/DateTimeHelper.cs
+++ PlexRequests.Helpers/DateTimeHelper.cs
@@ -8,14 +8,14 @@ namespace PlexRequests.Helpers
     {
         public static DateTimeOffset OffsetUTCDateTime(DateTime utcDateTime, int minuteOffset)
         {
-            //TimeSpan ts = TimeSpan.FromMinutes(-minuteOffset);
-            //return new DateTimeOffset(utcDateTime).ToOffset(ts);
+            TimeSpan ts = TimeSpan.FromMinutes(-minuteOffset);
+            return new DateTimeOffset(utcDateTime).ToOffset(ts);
 
             // this is a workaround below to work with MONO
-            var tzi = FindTimeZoneFromOffset(minuteOffset);
-            var utcOffset = tzi.GetUtcOffset(utcDateTime);
-            var newDate = utcDateTime + utcOffset;
-            return new DateTimeOffset(newDate.Ticks, utcOffset);
+            //var tzi = FindTimeZoneFromOffset(minuteOffset);
+            //var utcOffset = tzi.GetUtcOffset(utcDateTime);
+            //var newDate = utcDateTime + utcOffset;
+            //return new DateTimeOffset(newDate.Ticks, utcOffset);
         }
 
         public static void CustomParse(string date, out DateTime dt)
