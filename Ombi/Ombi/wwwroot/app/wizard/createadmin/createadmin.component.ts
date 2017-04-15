fa0e1676503c8ed47dddd722aad5dd39424043ba
tidusjar <jamie.rees@outlook.com>
2017-04-15 23:16:49 +0100

    small changes

---
+++ Ombi/Ombi/wwwroot/app/wizard/createadmin/createadmin.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../auth/auth.service';
+import { NotificationService } from '../../services/notification.service';
+
+@Component({
+    selector: 'ombi',
+    moduleId: module.id,
+    templateUrl: './plex.component.html',
+})
+export class PlexComponent {
+
+    constructor(private authService: AuthService,  private notificationService: NotificationService) { }
+    
+
+    username: string;
+    password: string;
+
+    createUser() {
+
+    }
+}
