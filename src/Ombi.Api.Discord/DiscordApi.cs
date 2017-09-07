391308ddeba0b5e7da573fde1553cba250b792bc
Jamie <tidusjar@gmail.com>
2017-09-07 09:37:54 +0100

    Update DiscordApi.cs

--- src/Ombi.Api.Discord/DiscordApi.cs
+++ src/Ombi.Api.Discord/DiscordApi.cs
@@ -11,7 +11,7 @@ namespace Ombi.Api.Discord
             Api = api;
         }
         
-        private string Endpoint => "https://discordapp.com/api/"; //webhooks/270828242636636161/lLysOMhJ96AFO1kvev0bSqP-WCZxKUh1UwfubhIcLkpS0DtM3cg4Pgeraw3waoTXbZii
+        private string Endpoint => "https://discordapp.com/api/";
         private IApi Api { get; }
         
         public async Task SendMessage(DiscordWebhookBody body, string webhookId, string webhookToken)
