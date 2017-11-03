7dccd63ff0c4f87e4be752f0cf9b88325a618c64
tidusjar <jamie.rees@outlook.com>
2017-11-03 22:53:23 +0000

    Push out missing migration

---
+++ src/Ombi.Store/Migrations/20171103224757_MovieBackground.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Ombi.Store.Migrations
+{
+    public partial class MovieBackground : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Background",
+                table: "MovieRequests",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Background",
+                table: "MovieRequests");
+        }
+    }
+}
