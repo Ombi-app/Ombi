dd306aae5a39d8fbd220b1e87f1b111a8fd33524
tidusjar <jamie.rees@outlook.com>
2017-11-03 21:22:53 +0000

    Fixed telegram #1667

--- src/Ombi.Api.Telegram/TelegramApi.cs
+++ src/Ombi.Api.Telegram/TelegramApi.cs
@@ -12,18 +12,18 @@ namespace Ombi.Api.Telegram
         }
 
         private readonly IApi _api;
-        private const string BaseUrl = "https://api.telegram.org/bot";
+        private const string BaseUrl = "https://api.telegram.org/";
 
 
         public async Task Send(string message, string botApi, string chatId, string parseMode)
         {
-            var request = new Request($"{botApi}/sendMessage", BaseUrl, HttpMethod.Post);
-            request.AddQueryString("chat_id", chatId);
+            var request = new Request($"bot{botApi}/sendMessage", BaseUrl, HttpMethod.Post);
 
             var body = new
             {
                 text = message,
-                parse_mode = parseMode
+                parse_mode = parseMode,
+                chat_id= chatId
             };
 
             request.AddJsonBody(body);
