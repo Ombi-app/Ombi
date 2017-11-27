02cd6608d0f3acb7e2e0bdf39f6beddf21d7d78a
Jamie <tidusjar@gmail.com>
2017-11-27 16:29:07 +0000

    Fixed landing page

--- src/Ombi/Models/MediaSeverAvailibilityViewModel.cs
+++ src/Ombi/Models/MediaSeverAvailibilityViewModel.cs
@@ -5,7 +5,7 @@
         public int ServersAvailable { get; set; }
         public int ServersUnavailable { get; set; }
         public bool PartiallyDown => ServersUnavailable > 0 && ServersAvailable > 0;
-        public bool CompletlyDown => ServersUnavailable > 0 && ServersAvailable == 0;
+        public bool CompletelyDown => ServersUnavailable > 0 && ServersAvailable == 0;
         public bool FullyAvailable => ServersUnavailable == 0 && ServersAvailable > 0;
         public int TotalServers => ServersUnavailable + ServersAvailable;
     }
