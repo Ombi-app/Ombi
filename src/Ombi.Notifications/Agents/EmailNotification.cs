408a3b446d9315f8a528919ae2f552c98c5954b8
tidusjar <tidusjar@gmail.com>
2018-02-07 23:34:20 +0000

    Fixed merge from rebase !wip

--- src/Ombi.Notifications/Agents/EmailNotification.cs
+++ src/Ombi.Notifications/Agents/EmailNotification.cs
@@ -21,7 +21,7 @@ namespace Ombi.Notifications.Agents
     public class EmailNotification : BaseNotification<EmailNotificationSettings>, IEmailNotification
     {
         public EmailNotification(ISettingsService<EmailNotificationSettings> settings, INotificationTemplatesRepository r, IMovieRequestRepository m, ITvRequestRepository t, IEmailProvider prov, ISettingsService<CustomizationSettings> c,
-            ILogger<EmailNotification> log, UserManager<OmbiUser> um) : base(settings, r, m, t, c)
+            ILogger<EmailNotification> log, UserManager<OmbiUser> um) : base(settings, r, m, t, c, log)
         {
             EmailProvider = prov;
             Logger = log;
