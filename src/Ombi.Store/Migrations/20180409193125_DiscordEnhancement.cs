bf3090be0177aeee374c3212ea8679dc4c139cca
Jamie Rees <Jamie.Rees@moneybarn.com>
2018-04-09 20:32:29 +0100

    Added db migrations that were missing

---
+++ src/Ombi.Store/Migrations/20180409193125_DiscordEnhancement.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Ombi.Store.Migrations
+{
+    public partial class DiscordEnhancement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Compact",
+                table: "NotificationTemplates",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Compact",
+                table: "NotificationTemplates");
+        }
+    }
+}
