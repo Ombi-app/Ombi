0000ff1ce90ffcacbd9e8f9f8c7b692a664f763e
Anojh Thayaparan <anojh@hotmail.com>
2018-04-16 06:47:01 -0700

    Inject base url if set before theme file url, see issue #1795 (#2148)

--- src/Ombi/Controllers/SettingsController.cs
+++ src/Ombi/Controllers/SettingsController.cs
@@ -274,6 +274,12 @@ namespace Ombi.Controllers
         public async Task<IActionResult> GetThemeContent([FromQuery]string url)
         {
             var css = await _githubApi.GetThemesRawContent(url);
+            var ombiSettings = await OmbiSettings();
+            if (ombiSettings.BaseUrl != null)
+            {
+                int index = css.IndexOf("/api/");
+                css = css.Insert(index, ombiSettings.BaseUrl);
+            }
             return Content(css, "text/css");
         }
 
