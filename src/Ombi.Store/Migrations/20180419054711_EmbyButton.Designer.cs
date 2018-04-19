893d72521799d278cd65fd8a183f445fbf685ae4
Anojh <anojh@hotmail.com>
2018-04-19 00:33:31 -0700

    Added Emby content url property

--- src/Ombi.Store/Migrations/20180419054711_EmbyButton.Designer.cs
+++ src/Ombi.Store/Migrations/20180419054711_EmbyButton.Designer.cs
@@ -14,9 +14,10 @@ using System;
 namespace Ombi.Store.Migrations
 {
     [DbContext(typeof(OmbiContext))]
-    partial class OmbiContextModelSnapshot : ModelSnapshot
+    [Migration("20180419054711_EmbyButton")]
+    partial class EmbyButton
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
