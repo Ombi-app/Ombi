712f7d5066245a13f20e6756514d0c713d0c567e
Anojh <anojh@hotmail.com>
2018-04-19 00:35:36 -0700

    Added View on Emby button to frontend and did UI work

--- src/Ombi/ClientApp/app/search/tvsearch.component.ts
+++ src/Ombi/ClientApp/app/search/tvsearch.component.ts
@@ -129,6 +129,7 @@ export class TvSearchComponent implements OnInit {
 
     public getExtraInfo() {
         this.tvResults.forEach((val, index) => {
+            console.log(val.data.id);
             this.imageService.getTvBanner(val.data.id).subscribe(x => {
                 val.data.background = this.sanitizer.
                 bypassSecurityTrustStyle
