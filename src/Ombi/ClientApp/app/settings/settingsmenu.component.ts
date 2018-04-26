ff0ad91a96f538d90154f7cac2e5590161ccbd31
Anojh <anojh@hotmail.com>
2018-04-26 15:17:38 -0700

    fixed settings nav item not retaining active class when in other tabs in the settings page

--- src/Ombi/ClientApp/app/settings/settingsmenu.component.ts
+++ src/Ombi/ClientApp/app/settings/settingsmenu.component.ts
@@ -7,4 +7,11 @@ export class SettingsMenuComponent {
     public ignore(event: any): void {
         event.preventDefault();
     }
+
+    public ngOnInit() {
+        var element = document.getElementById("settings");
+        if (element != null) {
+            element.classList.add("active");
+        }
+    }
  }
