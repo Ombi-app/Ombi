74a17773cfe48b932a29c59753d87bf7bd41d4d0
Jamie <tidusjar@gmail.com>
2018-06-28 21:37:51 +0100

    Added some new assembly nothing is going on here... !wip

---
+++ src/Ombi.Api.Lidarr/LidarrApi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Ombi.Api.Lidarr.Models;
+
+namespace Ombi.Api.Lidarr
+{
+    public class LidarrApi
+    {
+        public LidarrApi(ILogger<LidarrApi> logger, IApi api)
+        {
+            Api = api;
+            Logger = logger;
+        }
+
+        private IApi Api { get; }
+        private ILogger Logger { get; }
+
+        private const string ApiVersion = "/api/v1";
+
+        public async Task<List<LidarrProfile>> GetProfiles(string apiKey, string baseUrl)
+        {
+            var request = new Request($"{ApiVersion}/profile", baseUrl, HttpMethod.Get);
+
+            AddHeaders(request, apiKey);
+            return await Api.Request<List<LidarrProfile>>(request);
+        }
+
+        public async Task<List<LidarrRootFolder>> GetRootFolders(string apiKey, string baseUrl)
+        {
+            var request = new Request($"{ApiVersion}/rootfolder", baseUrl, HttpMethod.Get);
+
+            AddHeaders(request, apiKey);
+            return await Api.Request<List<LidarrRootFolder>>(request);
+        }
+
+        public async Task<List<AlbumLookup>> ArtistLookup(string searchTerm, string apiKey, string baseUrl)
+        {
+            var request = new Request($"{ApiVersion}/Artist/lookup", baseUrl, HttpMethod.Get);
+            request.AddQueryString("term", searchTerm);
+
+            AddHeaders(request, apiKey);
+            return await Api.Request<List<AlbumLookup>>(request);
+        }
+
+        public async Task<List<AlbumLookup>> AlbumLookup(string searchTerm, string apiKey, string baseUrl)
+        {
+            var request = new Request($"{ApiVersion}/Album/lookup", baseUrl, HttpMethod.Get);
+            request.AddQueryString("term", searchTerm);
+
+            AddHeaders(request, apiKey);
+            return await Api.Request<List<AlbumLookup>>(request);
+        }
+
+        private void AddHeaders(Request request, string key)
+        {
+            request.AddHeader("X-Api-Key", key);
+        }
+    }
+}
---
+++ src/Ombi.Api.Lidarr/Models/LidarrProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Ombi.Api.Lidarr.Models
+{
+    public class Cutoff
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Quality
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Item
+    {
+        public Quality quality { get; set; }
+        public bool allowed { get; set; }
+    }
+
+    public class LidarrProfile
+{
+        public string name { get; set; }
+        public Cutoff cutoff { get; set; }
+        public List<Item> items { get; set; }
+        public string language { get; set; }
+        public int id { get; set; }
+    }
+}
