636b6c2b027d3e2b0cbdd01a45bf56f4a91e4924
Jamie <tidusjar@gmail.com>
2018-08-01 21:22:18 +0100

    #2371 Fixed the issue where certain actions would not setup the series correctly in Sonarr

--- src/Ombi.Api.Sonarr/Models/Episode.cs
+++ src/Ombi.Api.Sonarr/Models/Episode.cs
@@ -6,6 +6,30 @@ namespace Ombi.Api.Sonarr.Models
 {
     public class Episode
     {
+        public Episode()
+        {
+            
+        }
+
+        public Episode(Episode ep)
+        {
+            seriesId = ep.seriesId;
+            episodeFileId = ep.episodeFileId;
+            seasonNumber = ep.seasonNumber;
+            episodeNumber = ep.episodeNumber;
+            title = ep.title;
+            airDate = ep.airDate;
+            airDateUtc = ep.airDateUtc;
+            overview = ep.overview;
+            hasFile = ep.hasFile;
+            monitored = ep.monitored;
+            unverifiedSceneNumbering = ep.unverifiedSceneNumbering;
+            id = ep.id;
+            absoluteEpisodeNumber = ep.absoluteEpisodeNumber;
+            sceneAbsoluteEpisodeNumber = ep.sceneAbsoluteEpisodeNumber;
+            sceneEpisodeNumber = ep.sceneEpisodeNumber;
+            sceneSeasonNumber = ep.sceneSeasonNumber;
+        }
         public int seriesId { get; set; }
         public int episodeFileId { get; set; }
         public int seasonNumber { get; set; }
@@ -27,6 +51,24 @@ namespace Ombi.Api.Sonarr.Models
 
     public class Episodefile
     {
+        public Episodefile()
+        {
+            
+        }
+
+        public Episodefile(Episodefile e)
+        {
+            seriesId = e.seriesId;
+            seasonNumber = e.seasonNumber;
+            relativePath = e.relativePath;
+            path = e.path;
+            size = e.size;
+            dateAdded = e.dateAdded;
+            sceneName = e.sceneName;
+            quality = new EpisodeQuality(e.quality);
+            qualityCutoffNotMet = e.qualityCutoffNotMet;
+            id = e.id;
+        }
         public int seriesId { get; set; }
         public int seasonNumber { get; set; }
         public string relativePath { get; set; }
@@ -41,12 +83,32 @@ namespace Ombi.Api.Sonarr.Models
 
     public class EpisodeQuality
     {
+        public EpisodeQuality()
+        {
+            
+        }
+
+        public EpisodeQuality(EpisodeQuality e)
+        {
+            quality = new Quality(e.quality);
+            revision = new Revision(e.revision);
+        }
         public Quality quality { get; set; }
         public Revision revision { get; set; }
     }
 
     public class Revision
     {
+        public Revision()
+        {
+            
+        }
+
+        public Revision(Revision r)
+        {
+            version = r.version;
+            real = r.real;
+        }
         public int version { get; set; }
         public int real { get; set; }
     }
