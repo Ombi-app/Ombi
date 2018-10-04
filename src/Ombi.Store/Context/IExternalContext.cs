479ca819d75f61144f7049c7f16feeca26b2a503
TidusJar <tidusjar@gmail.com>
2018-10-04 14:48:21 +0100

    Added in the external repo so we can rip out external stuff

---
+++ src/Ombi.Store/Context/IExternalContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ombi.Store.Entities;
+
+namespace Ombi.Store.Context
+{
+    public interface IExternalContext : IDbContext
+    {
+        DbSet<CouchPotatoCache> CouchPotatoCache { get; set; }
+        DbSet<EmbyContent> EmbyContent { get; set; }
+        DbSet<EmbyEpisode> EmbyEpisode { get; set; }
+        DbSet<LidarrAlbumCache> LidarrAlbumCache { get; set; }
+        DbSet<LidarrArtistCache> LidarrArtistCache { get; set; }
+        DbSet<PlexEpisode> PlexEpisode { get; set; }
+        DbSet<PlexServerContent> PlexServerContent { get; set; }
+        DbSet<RadarrCache> RadarrCache { get; set; }
+        DbSet<SickRageCache> SickRageCache { get; set; }
+        DbSet<SickRageEpisodeCache> SickRageEpisodeCache { get; set; }
+        DbSet<SonarrCache> SonarrCache { get; set; }
+        DbSet<SonarrEpisodeCache> SonarrEpisodeCache { get; set; }
+    }
+}
---
+++ src/Ombi.Store/Repository/IExternalRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Ombi.Store.Entities;
+
+namespace Ombi.Store.Repository
+{
+    public interface IExternalRepository<T> : IDisposable where T : Entity
+    {
+        Task<T> Find(object key);
+        IQueryable<T> GetAll();
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task AddRange(IEnumerable<T> content, bool save = true);
+        Task<T> Add(T content);
+        Task DeleteRange(IEnumerable<T> req);
+        Task Delete(T request);
+        Task<int> SaveChangesAsync();
+
+        IIncludableQueryable<TEntity, TProperty> Include<TEntity, TProperty>(
+            IQueryable<TEntity> source, Expression<Func<TEntity, TProperty>> navigationPropertyPath)
+            where TEntity : class;
+
+        Task ExecuteSql(string sql);
+        DbSet<T> _db { get; }
+    }
+}
