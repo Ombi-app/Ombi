b1e6fd313ba51646d31c53fb3bbf39af17105007
TidusJar <tidusjar@gmail.com>
2018-11-14 20:06:50 +0000

    !wip started on the request queue

---
+++ src/Ombi.Schedule/Jobs/Ombi/ResendFailedRequests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Ombi.Store.Entities;
+using Ombi.Store.Repository;
+
+namespace Ombi.Schedule.Jobs.Ombi
+{
+    public class ResendFailedRequests
+    {
+        public ResendFailedRequests(IRepository<RequestQueue> queue, IMovieSender movieSender)
+        {
+            
+        }
+
+        public async Task Start()
+        {
+
+        }
+    }
+}
