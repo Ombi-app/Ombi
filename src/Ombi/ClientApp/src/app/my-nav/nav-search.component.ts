857e388dcbfac2e36a9326ed5a9b43252f274193
TidusJar <tidusjar@gmail.com>
2019-01-28 12:17:23 +0000

    Searchbar !wip

--- src/Ombi/ClientApp/src/app/my-nav/nav-search.component.ts
+++ src/Ombi/ClientApp/src/app/my-nav/nav-search.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
-import { Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 import { SearchV2Service } from '../services/searchV2.service';
 import { IMultiSearchResult } from '../interfaces';
-import { MatAutocompleteSelectedEvent } from '@angular/material';
 import { Router } from '@angular/router';
+import { NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-nav-search',
@@ -14,28 +14,33 @@ import { Router } from '@angular/router';
 })
 export class NavSearchComponent {
 
-    public searchChanged: Subject<string> = new Subject<string>();
-    public searchText: string;
-    public searchResult: IMultiSearchResult[];
+    public selectedItem: string;
+
+    
+    public searching = false;
+    public searchFailed = false;
+    
+    
+    public formatter = (result: IMultiSearchResult) => result.media_type == "movie" ? result.title : result.name;
+    
+    public searchModel = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(600),
+      distinctUntilChanged(),
+      switchMap(term =>
+        this.searchService.multiSearch(term)
+      )
+    )
 
     constructor(private searchService: SearchV2Service, private router: Router) {
-        this.searchChanged.pipe(
-            debounceTime(600), // Wait Xms after the last event before emitting last event
-            distinctUntilChanged(), // only emit if value is different from previous value
-        ).subscribe(x => {
-            this.searchText = x as string;
-            this.searchService.multiSearch(this.searchText).subscribe(x => this.searchResult = x)
-        });
+
     }
 
-    public search(text: any) {
-        this.searchChanged.next(text.target.value);
-    }
+  
 
-    public selected(option: MatAutocompleteSelectedEvent) {
-        var selected = option.option.value as IMultiSearchResult;
-        if (selected.media_type == "movie") {
-            this.router.navigate([`details/movie/${selected.id}`]);
+    public selected(event: NgbTypeaheadSelectItemEvent) {
+        if (event.item.media_type == "movie") {
+            this.router.navigate([`details/movie/${event.item.id}`]);
             return;
         }
     }
