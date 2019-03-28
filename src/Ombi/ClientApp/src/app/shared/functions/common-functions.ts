ce6dd5e2ee46559b4a87897c05ae592d7455cca2
Jamie Rees <tidusjar@gmail.com>
2019-03-28 11:28:13 +0000

    Added the rest of the URLS

--- src/Ombi/ClientApp/src/app/shared/functions/common-functions.ts
+++ src/Ombi/ClientApp/src/app/shared/functions/common-functions.ts
@@ -2,19 +2,26 @@ export function getBaseLocation() {
     debugger;
     let paths: string[] = location.pathname.split('/').splice(1, 1);
     let basePath: string = (paths && paths[0] ? paths[0] : ""); 
-    if(invalidProxies.indexOf(basePath) === -1){
+    if(invalidProxies.indexOf(basePath.toUpperCase()) === -1){
         return '/';
     }
     return '/' + basePath;
 }
 
 const invalidProxies: string[] = [
-    'discover',
-    'requests-list',
-    'Settings',
-    'issues',
-    'usermanagement',
-    'recentlyadded',
-    'details',
-    'vote'
+    'DISCOVER',
+    'REQUESTS-LIST',
+    'SETTINGS',
+    'ISSUES',
+    'USERMANAGEMENT',
+    'RECENTLYADDED',
+    'DETAILS',
+    'VOTE',
+    'LOGIN',
+    'LANDINGPAGE',
+    'TOKEN',
+    'RESET',
+    'CUSTOM',
+    'AUTH',
+    'WIZARD'    
 ]
