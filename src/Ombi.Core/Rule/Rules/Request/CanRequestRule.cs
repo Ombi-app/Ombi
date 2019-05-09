6c5e359d6919466d253428ca54133341dceff027
Patrick Collins <PatrickCP0@Gmail.com>
2019-05-09 04:23:04 -0400

    add braces to all if statements

--- src/Ombi.Core/Rule/Rules/Request/CanRequestRule.cs
+++ src/Ombi.Core/Rule/Rules/Request/CanRequestRule.cs
@@ -19,26 +19,37 @@ namespace Ombi.Core.Rule.Rules
         public Task<RuleResult> Execute(BaseRequest obj)
         {
             if (User.IsInRole(OmbiRoles.Admin))
+            {
                 return Task.FromResult(Success());
+            }
 
             if (obj.RequestType == RequestType.Movie)
             {
                 if (User.IsInRole(OmbiRoles.RequestMovie) || User.IsInRole(OmbiRoles.AutoApproveMovie))
+                {
                     return Task.FromResult(Success());
+                }
+
                 return Task.FromResult(Fail("You do not have permissions to Request a Movie"));
             }
 
             if (obj.RequestType == RequestType.TvShow)
             {
                 if (User.IsInRole(OmbiRoles.RequestTv) || User.IsInRole(OmbiRoles.AutoApproveTv))
+                {
                     return Task.FromResult(Success());
+                }
+
                 return Task.FromResult(Fail("You do not have permissions to Request a TV Show"));
             }
 
             if (obj.RequestType == RequestType.Album)
             {
                 if (User.IsInRole(OmbiRoles.RequestMusic) || User.IsInRole(OmbiRoles.AutoApproveMusic))
+                {
                     return Task.FromResult(Success());
+                }
+
                 return Task.FromResult(Fail("You do not have permissions to Request an Album"));
             }
 
