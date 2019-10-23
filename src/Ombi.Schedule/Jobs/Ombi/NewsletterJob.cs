23cceec6f90b04d5572e3e0113cd5029ebb80e46
tidusjar <tidusjar@gmail.com>
2019-10-23 08:50:46 +0100

    Fixed potential null ref !wip

--- src/Ombi.Schedule/Jobs/Ombi/NewsletterJob.cs
+++ src/Ombi.Schedule/Jobs/Ombi/NewsletterJob.cs
@@ -715,7 +715,7 @@ namespace Ombi.Schedule.Jobs.Ombi
                         {
                             SeasonNumber = key,
                             Episodes = g.ToList(),
-                            EpisodeAirDate = tvInfo.seasons.Where(x => x.season_number == key).Select(x => x.air_date)
+                            EpisodeAirDate = tvInfo?.seasons?.Where(x => x.season_number == key)?.Select(x => x.air_date) ?? new List<string>()
                         }
                     );
 
@@ -840,7 +840,7 @@ namespace Ombi.Schedule.Jobs.Ombi
                         {
                             SeasonNumber = key,
                             Episodes = g.ToList(),
-                            EpisodeAirDate = tvInfo.seasons.Where(x => x.season_number == key).Select(x => x.air_date)
+                            EpisodeAirDate = tvInfo?.seasons?.Where(x => x.season_number == key)?.Select(x => x.air_date) ?? new List<string>()
                         }
                     );
 
