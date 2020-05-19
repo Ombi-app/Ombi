78b55db384c8e96fb6845fd176b7b8facf86c8be
Jamie Rees <jamie.rees@myairops.com>
2020-05-19 22:09:19 +0100

    re-ordered titles

--- src/Ombi/ClientApp/src/app/requests-list/components/movies-grid/movies-grid.component.ts
+++ src/Ombi/ClientApp/src/app/requests-list/components/movies-grid/movies-grid.component.ts
@@ -19,7 +19,7 @@ export class MoviesGridComponent implements OnInit, AfterViewInit {
     public dataSource: IMovieRequests[] = [];
     public resultsLength: number;
     public isLoadingResults = true;
-    public displayedColumns: string[] = ['requestedUser.requestedBy', 'title', 'requestedDate', 'status', 'requestStatus', 'actions'];
+    public displayedColumns: string[] = ['title', 'requestedUser.requestedBy',  'status', 'requestStatus','requestedDate', 'actions'];
     public gridCount: string = "15";
     public isAdmin: boolean;
     public defaultSort: string = "requestedDate";
