ab15439a27bdda3c9eb61e218cb689b25bacec63
Jamie Rees <jamie.rees@myairops.com>
2020-06-18 22:19:01 +0100

    Added images to the search

--- src/Ombi.Core/Models/Search/V2/MultiSearchResult.cs
+++ src/Ombi.Core/Models/Search/V2/MultiSearchResult.cs
@@ -5,5 +5,6 @@
         public string Id { get; set; }
         public string MediaType { get; set; }
         public string Title { get; set; }
+        public string Poster { get; set; }
     }
 }
