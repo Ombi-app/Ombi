ba0962e0188c0034af0c5c302da832bd54fb9f8c
Jamie Rees <jamie.rees@myairops.com>
2020-06-20 22:23:10 +0100

    Made some changes to the cast list from reddit feedback

--- src/Ombi/ClientApp/src/app/media-details/components/shared/cast-carousel/cast-carousel.component.ts
+++ src/Ombi/ClientApp/src/app/media-details/components/shared/cast-carousel/cast-carousel.component.ts
@@ -6,6 +6,27 @@ import { Component, Input } from "@angular/core";
     styleUrls: ["./cast-carousel.component.scss"]
 })
 export class CastCarouselComponent {
-    
+
+    constructor() {
+        this.responsiveOptions = [
+            {
+                breakpoint: '1024px',
+                numVisible: 5,
+                numScroll: 5
+            },
+            {
+                breakpoint: '768px',
+                numVisible: 3,
+                numScroll: 3
+            },
+            {
+                breakpoint: '560px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
+    }
+
     @Input() cast: any[];
+    public responsiveOptions: any[];
 }
