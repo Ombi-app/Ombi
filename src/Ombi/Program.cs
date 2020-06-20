913197ac1840365052ed9e96d90faa7a6d45545e
Jamie Rees <jamie.rees@myairops.com>
2020-06-20 23:08:02 +0100

    Fixed the host args

--- src/Ombi/Program.cs
+++ src/Ombi/Program.cs
@@ -176,7 +176,7 @@ namespace Ombi
         [Option("host", Required = false, HelpText =
             "Set to a semicolon-separated (;) list of URL prefixes to which the server should respond. For example, http://localhost:123." +
             " Use \"localhost\" to indicate that the server should listen for requests on any IP address or hostname using the specified port and protocol (for example, http://localhost:5000). " +
-            "The protocol (http:// or https://) must be included with each URL. Supported formats vary between servers.", Default = "http://localhost:5000")]
+            "The protocol (http:// or https://) must be included with each URL. Supported formats vary between servers.", Default = "http://*:5000")]
         public string Host { get; set; }
 
         [Option("storage", Required = false, HelpText = "Storage path, where we save the logs and database")]
