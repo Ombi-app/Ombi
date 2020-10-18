8f8c46c5b120b0117dc6bfc659dfceddc8df7733
adamworley <worley9@hotmail.co.uk>
2020-10-18 09:46:53 +0100

    Add TvSeason and Episode models

--- src/Ombi.TheMovieDbApi/Models/TvInfo.cs
+++ src/Ombi.TheMovieDbApi/Models/TvInfo.cs
@@ -74,6 +74,33 @@ namespace Ombi.Api.TheMovieDb.Models
         public int season_number { get; set; }
     }
 
+    public class TvSeason
+    {
+        public string _id { get; set; }
+        public string air_date { get; set; }
+        public Episode[] episodes { get; set; }
+        public string name { get; set; }
+        public string overview { get; set; }
+        public int id { get; set; }
+        public string poster_path { get; set; }
+        public int season_number { get; set; }
+    }
+
+    public class Episode
+    {
+        public string air_date { get; set; }
+        public Crew[] crew { get; set; }
+        public int episode_number { get; set; }
+        public string name { get; set; }
+        public string overview { get; set; }
+        public int id { get; set; }
+        public string production_code { get; set; }
+        public int season_number { get; set; }
+        public string still_path { get; set; }
+        public float vote_average { get; set; }
+        public int vote_count { get; set; }
+    }
+
     public class TvExternalIds
     {
         [JsonProperty("imdb_id")] public string ImdbId { get; set; }
