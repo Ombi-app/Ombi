cd99a876c979d7772b38eca3beddf1c1cb2efc59
tidusjar <tidusjar@gmail.com>
2021-01-30 23:22:40 +0000

    Moved the videos into a carousel and outside of the accordion on the movies page

---
+++ src/Ombi/Controllers/V2/WizardController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Ombi.Attributes;
+using Ombi.Core.Settings;
+using Ombi.Settings.Settings.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ombi.Controllers.V2
+{
+    [ServiceFilter(typeof(WizardActionFilter))]
+    [AllowAnonymous]
+    public class WizardController : V2Controller
+    {
+
+        private ISettingsService<OmbiSettings> _ombiSettings { get; }
+        
+
+        [HttpGet]
+        public IActionResult Ok()
+        {
+            return Ok();
+        }
+
+
+
+
+    }
+
+
+}
