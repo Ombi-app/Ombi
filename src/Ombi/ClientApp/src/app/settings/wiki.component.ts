197fc5af61c5afb663403dad8396f135593621b2
tidusjar <tidusjar@gmail.com>
2021-06-17 08:06:00 +0100

    The docs button now go to the specific doc's pages #4223

--- src/Ombi/ClientApp/src/app/settings/wiki.component.ts
+++ src/Ombi/ClientApp/src/app/settings/wiki.component.ts
@@ -1,11 +1,18 @@
 ﻿import { Component, Input } from "@angular/core";
 
+import { Router } from "@angular/router";
+
 @Component({
     selector:"wiki",
     templateUrl: "./wiki.component.html",
 })
 export class WikiComponent {
-    @Input() public path: string;
     @Input() public text: string;
-    public domain: string = "http://docs.ombi.app/"
+    public domain: string = "http://docs.ombi.app"
+
+    get url(): string {
+        return this.router.url.toLowerCase();
+    }
+
+    constructor(private router: Router) { }
 }
