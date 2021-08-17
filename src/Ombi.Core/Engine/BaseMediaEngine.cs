45b692c8d216d4e3367c6a9b80843cf3fd5165d5
tidusjar <tidusjar@gmail.com>
2021-08-17 08:27:10 +0100

    Fixed #4290

--- src/Ombi.Core/Engine/BaseMediaEngine.cs
+++ src/Ombi.Core/Engine/BaseMediaEngine.cs
@@ -173,6 +173,10 @@ namespace Ombi.Core.Engine
                 return currentCode;
             }
             var user = await GetUser();
+            if (user == null)
+            {
+                return "en";
+            }
 
             if (string.IsNullOrEmpty(user.Language))
             {
