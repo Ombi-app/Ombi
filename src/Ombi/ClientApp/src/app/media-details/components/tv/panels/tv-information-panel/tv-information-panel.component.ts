99c728918e74805a0b2ecdb72db41d45b57b619e
tidusjar <tidusjar@gmail.com>
2021-09-08 21:15:40 +0100

    fix(details): :alien: Removed the RT API calls from the frontend for now
    #4305

--- src/Ombi/ClientApp/src/app/media-details/components/tv/panels/tv-information-panel/tv-information-panel.component.ts
+++ src/Ombi/ClientApp/src/app/media-details/components/tv/panels/tv-information-panel/tv-information-panel.component.ts
@@ -31,8 +31,8 @@ export class TvInformationPanelComponent implements OnInit {
         if (this.internalBaseUrl.length > 1) {
             this.baseUrl = this.internalBaseUrl;
         }
-        this.searchService.getRottenTvRatings(this.tv.title, +this.tv.firstAired.toString().substring(0,4))
-            .subscribe(x => this.ratings = x);
+        // this.searchService.getRottenTvRatings(this.tv.title, +this.tv.firstAired.toString().substring(0,4))
+        //     .subscribe(x => this.ratings = x);
 
         this.searchService.getTvStreams(+this.tv.id ).subscribe(x => this.streams = x);
         this.tv.seasonRequests.forEach(season => {
