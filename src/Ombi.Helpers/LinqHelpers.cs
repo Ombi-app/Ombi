4a03ecc8a01871bb01b22e680d0335e07dbd3af3
tidusjar <tidusjar@gmail.com>
2021-11-09 16:56:14 +0000

    chore: updated main packages

--- src/Ombi.Helpers/LinqHelpers.cs
+++ src/Ombi.Helpers/LinqHelpers.cs
@@ -6,16 +6,6 @@ namespace Ombi.Helpers
 {
     public static class LinqHelpers
     {
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-        {
-            HashSet<TKey> knownKeys = new HashSet<TKey>();
-            foreach (TSource source1 in source)
-            {
-                if (knownKeys.Add(keySelector(source1)))
-                    yield return source1;
-            }
-        }
-
         public static HashSet<T> ToHashSet<T>(
             this IEnumerable<T> source,
             IEqualityComparer<T> comparer = null)
