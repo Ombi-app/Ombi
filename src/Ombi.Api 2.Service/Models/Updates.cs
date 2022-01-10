4665346f93fe0f4ef57d4617845ad349cd019971
Paannda <simon.mera@gmail.com>
2022-01-09 20:42:22 -0500

    Rebase with latest develop

---
+++ src/Ombi.Api 2.Service/Models/Updates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Ombi.Api.Service.Models
+{
+
+    public class Updates
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("updateVersionString")]
+        public string UpdateVersionString { get; set; }
+        [JsonProperty("updateVersion")]
+        public int UpdateVersion { get; set; }
+        [JsonProperty("updateDate")]
+        public DateTime UpdateDate { get; set; }
+        [JsonProperty("changeLogs")]
+        public List<Changelog> ChangeLogs { get; set; }
+        [JsonProperty("downloads")]
+        public List<Download> Downloads { get; set; }
+    }
+
+    public class Changelog
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("descripion")]
+        public string Descripion { get; set; }
+        [JsonProperty("updateId")]
+        public int UpdateId { get; set; }
+    }
+
+    public class Download
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("updateId")]
+        public int UpdateId { get; set; }
+    }
+}
---
+++ src/Ombi.Settings/Settings/Models/LdapSettings 2.cs
@@ -0,0 +1,81 @@
+﻿namespace Ombi.Settings.Settings.Models
+{
+    public class LdapSettings : Settings
+    {
+        public LdapSettings()
+        {
+            IsEnabled = false;
+            CreateUsersAtLogin = true;
+            Hostname = "ldap-server.example.tld";
+            BaseDn = "o=domains,dc=example,dc=tld";
+            Port = 389;
+            UsernameAttribute = "uid";
+            SearchFilter = "(memberOf=cn=Users,dc=example,dc=tld)";
+            BindUserDn = "cn=BindUser,dc=example,dc=tld";
+            BindUserPassword = "password";
+            UseSsl = true;
+            UseStartTls = false;
+            SkipSslVerify = false;
+        }
+
+        /// <summary>
+        /// Gets or sets whether LDAP is enabled.
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether users should be automatically created at login.
+        /// </summary>
+        public bool CreateUsersAtLogin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ldap server ip or url.
+        /// </summary>
+        public string Hostname { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ldap base search dn.
+        /// </summary>
+        public string BaseDn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ldap port.
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ldap username attribute.
+        /// </summary>
+        public string UsernameAttribute { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ldap user search filter.
+        /// </summary>
+        public string SearchFilter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ldap bind user dn.
+        /// </summary>
+        public string BindUserDn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ldap bind user password.
+        /// </summary>
+        public string BindUserPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use ssl when connecting to the ldap server.
+        /// </summary>
+        public bool UseSsl { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use StartTls when connecting to the ldap server.
+        /// </summary>
+        public bool UseStartTls { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to skip ssl verification.
+        /// </summary>
+        public bool SkipSslVerify { get; set; }
+    }
+}
