7b13994359afcb819045c5070852600caf883932
Florian Dupret <34862846+sephrat@users.noreply.github.com>
2022-02-05 09:49:26 +0100

    Refactor Emby and Jellyfin provider ids

--- src/Ombi.Api.Emby/Models/Media/EmbyProviderids.cs
+++ src/Ombi.Api.Emby/Models/Media/EmbyProviderids.cs
@@ -1,27 +1,11 @@
-﻿namespace Ombi.Api.Emby.Models.Movie
-{
-    public class EmbyProviderids
-    {
-        public string Tmdb { get; set; }
-        public string Imdb { get; set; }
-        public string TmdbCollection { get; set; }
+﻿using Ombi.Api.MediaServer.Models;
 
-        public string Tvdb { get; set; }
+namespace Ombi.Api.Emby.Models.Movie
+{
+    public class EmbyProviderids: BaseProviderids
+    {
+        public string TmdbCollection { get; set; }
         public string Zap2It { get; set; }
         public string TvRage { get; set; }
-        
-        public bool Any()
-        {
-            if (string.IsNullOrEmpty(Imdb)
-               && string.IsNullOrEmpty(Tmdb)
-               && string.IsNullOrEmpty(Tvdb))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
--- src/Ombi.Api.Jellyfin/Models/Media/JellyfinProviderids.cs
+++ src/Ombi.Api.Jellyfin/Models/Media/JellyfinProviderids.cs
@@ -1,27 +1,11 @@
-﻿namespace Ombi.Api.Jellyfin.Models.Movie
-{
-    public class JellyfinProviderids
-    {
-        public string Tmdb { get; set; }
-        public string Imdb { get; set; }
-        public string TmdbCollection { get; set; }
+﻿using Ombi.Api.MediaServer.Models;
 
-        public string Tvdb { get; set; }
+namespace Ombi.Api.Jellyfin.Models.Movie
+{
+    public class JellyfinProviderids: BaseProviderids
+    {
+        public string TmdbCollection { get; set; }
         public string Zap2It { get; set; }
         public string TvRage { get; set; }
-        
-        public bool Any()
-        {
-            if (string.IsNullOrEmpty(Imdb)
-               && string.IsNullOrEmpty(Tmdb)
-               && string.IsNullOrEmpty(Tvdb))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
