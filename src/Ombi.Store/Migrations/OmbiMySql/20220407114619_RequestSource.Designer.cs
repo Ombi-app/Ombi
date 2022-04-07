75356b44c7aea564e6d5df428822636c7603d66a
tidusjar <tidusjar@gmail.com>
2022-04-07 12:52:09 +0100

    Added migrations for mysql

--- src/Ombi.Store/Migrations/OmbiMySql/20220407114619_RequestSource.Designer.cs
+++ src/Ombi.Store/Migrations/OmbiMySql/20220407114619_RequestSource.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ombi.Store.Context.MySql;
 
@@ -10,9 +11,10 @@ using Ombi.Store.Context.MySql;
 namespace Ombi.Store.Migrations.OmbiMySql
 {
     [DbContext(typeof(OmbiMySqlContext))]
-    partial class OmbiMySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20220407114619_RequestSource")]
+    partial class RequestSource
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Ombi.Store/Migrations/OmbiMySql/20220407114619_RequestSource.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ombi.Store.Migrations.OmbiMySql
+{
+    public partial class RequestSource : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Source",
+                table: "MovieRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Source",
+                table: "ChildRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MediaServerToken",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Source",
+                table: "AlbumRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "MovieRequests");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "ChildRequests");
+
+            migrationBuilder.DropColumn(
+                name: "MediaServerToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "AlbumRequests");
+        }
+    }
+}
