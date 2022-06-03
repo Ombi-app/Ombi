710edbc2f05a1fca6605679ac25bfa6d3cf39f00
tidusjar <tidusjar@gmail.com>
2022-06-03 21:28:22 +0100

    refactor: :fire: removed angular-bootstrap-md dependancy

--- src/Ombi/ClientApp/src/app/search/search.module.ts
+++ src/Ombi/ClientApp/src/app/search/search.module.ts
@@ -1,27 +1,21 @@
-﻿import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterModule, Routes } from "@angular/router";
+﻿import { RouterModule, Routes } from "@angular/router";
 
-
-import { MovieSearchComponent } from "./moviesearch.component";
-import { MovieSearchGridComponent } from "./moviesearchgrid.component";
 import { AlbumSearchComponent } from "./music/albumsearch.component";
 import { ArtistSearchComponent } from "./music/artistsearch.component";
-import { MusicSearchComponent } from "./music/musicsearch.component";
-import { SearchComponent } from "./search.component";
-import { SeriesInformationComponent } from "./seriesinformation.component";
-import { TvSearchComponent } from "./tvsearch.component";
-
-import { CardsFreeModule } from "angular-bootstrap-md";
-
-import { RequestService } from "../services";
-import { SearchService } from "../services";
-
 import { AuthGuard } from "../auth/auth.guard";
-
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { MovieSearchComponent } from "./moviesearch.component";
+import { MovieSearchGridComponent } from "./moviesearchgrid.component";
+import { MusicSearchComponent } from "./music/musicsearch.component";
+import { NgModule } from "@angular/core";
 import { RemainingRequestsComponent } from "../requests/remainingrequests.component";
+import { RequestService } from "../services";
+import { SearchComponent } from "./search.component";
+import { SearchService } from "../services";
+import { SeriesInformationComponent } from "./seriesinformation.component";
 import { SharedModule } from "../shared/shared.module";
+import { TvSearchComponent } from "./tvsearch.component";
 
 const routes: Routes = [
     { path: "", component: SearchComponent, canActivate: [AuthGuard] },
@@ -36,7 +30,6 @@ const routes: Routes = [
         SharedModule,
         SidebarModule,
         TooltipModule,
-        CardsFreeModule,
     ],
     declarations: [
         SearchComponent,
