7c516d6b1bb89348642e98107f4dc8b6f812257d
tidusjar <tidusjar@gmail.com>
2023-01-13 20:46:01 +0000

    Tests working

--- src/Ombi.Tests/Middlewear/ApiKeyMiddlewearTests.cs
+++ src/Ombi.Tests/Middlewear/ApiKeyMiddlewearTests.cs
@@ -2,7 +2,9 @@
 using Moq;
 using Moq.AutoMock;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 using Ombi.Core.Authentication;
+using Ombi.Test.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,15 +45,50 @@ namespace Ombi.Tests.Middlewear
             var context = GetContext();
             context.Request.Path = "/api";
             context.Request.Headers.Add("UserAccessToken", new Microsoft.Extensions.Primitives.StringValues("test"));
-
-            var umMock = new Mock<OmbiUserManager>();
-            _mocker.Setup<IServiceProvider, OmbiUserManager>(x => (OmbiUserManager)x.GetService(typeof(OmbiUserManager)))
+            var user = new Store.Entities.OmbiUser
+            {
+                UserAccessToken = "test",
+                UserName = "unit test"
+            };
+            var umMock = MockHelper.MockUserManager(new List<Store.Entities.OmbiUser>
+            {
+               user
+            });
+            umMock.Setup(x => x.GetRolesAsync(user)).ReturnsAsync(new List<string> { "Admin" });
+            _mocker.Setup<IServiceProvider, object?>(x => x.GetService(typeof(OmbiUserManager)))
                 .Returns(umMock.Object);
-            
-            
+
+
             await _subject.Invoke(context);
 
-            _mocker.Verify<IServiceProvider>(x => x.GetService(It.IsAny<Type>()), Times.Never);
+            _mocker.Verify<IServiceProvider>(x => x.GetService(It.IsAny<Type>()), Times.Once);
+            umMock.Verify(x => x.UpdateAsync(user), Times.Once);
+        }
+
+        [Test]
+        public async Task ValidateUserAccessToken_Token_Invalid()
+        {
+            var context = GetContext();
+            context.Request.Path = "/api";
+            context.Request.Headers.Add("UserAccessToken", new Microsoft.Extensions.Primitives.StringValues("invalid"));
+            var user = new Store.Entities.OmbiUser
+            {
+                UserAccessToken = "test",
+                UserName = "unit test"
+            };
+            var umMock = MockHelper.MockUserManager(new List<Store.Entities.OmbiUser>
+            {
+               user
+            });
+            umMock.Setup(x => x.GetRolesAsync(user)).ReturnsAsync(new List<string> { "Admin" });
+            _mocker.Setup<IServiceProvider, object?>(x => x.GetService(typeof(OmbiUserManager)))
+                .Returns(umMock.Object);
+
+
+            await _subject.Invoke(context);
+
+            Assert.That(context.Response.StatusCode, Is.EqualTo(401));
+            umMock.Verify(x => x.UpdateAsync(user), Times.Never);
         }
 
         private HttpContext GetContext()
