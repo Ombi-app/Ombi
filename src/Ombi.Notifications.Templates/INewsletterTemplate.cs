e2971f821e236234bfbd604f4ffe0608247a1f19
Avi <357984+Unimatrix0@users.noreply.github.com>
2023-11-07 17:12:23 -0600

    Update INewsletterTemplate.cs
    Add applicationUrl

--- src/Ombi.Notifications.Templates/INewsletterTemplate.cs
+++ src/Ombi.Notifications.Templates/INewsletterTemplate.cs
@@ -2,6 +2,6 @@
 {
     public interface INewsletterTemplate
     {
-        string LoadTemplate(string subject, string intro, string tableHtml, string logo, string unsubscribeLink);
+        string LoadTemplate(string subject, string intro, string tableHtml, string logo, string unsubscribeLink, string applicationUrl);
     }
 }
