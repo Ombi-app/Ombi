a3688b6ad71d3b3cfd190a71ce12e1cb002e8d10
tidusjar <tidusjar@gmail.com>
2024-03-06 09:42:04 +0000

    Fixed the dates

--- src/Ombi/ClientApp/src/app/pipes/pipe.module.ts
+++ src/Ombi/ClientApp/src/app/pipes/pipe.module.ts
@@ -5,11 +5,14 @@ import { ThousandShortPipe } from './ThousandShortPipe';
 import { SafePipe } from './SafePipe';
 import { QualityPipe } from './QualityPipe';
 import { OrderPipe } from './OrderPipe';
+import { OmbiDatePipe } from './OmbiDatePipe';
+import { FormatPipeModule, FormatPipe } from 'ngx-date-fns';
 
 @NgModule({
-	imports: [],
-	declarations: [HumanizePipe, ThousandShortPipe, SafePipe, QualityPipe, TranslateStatusPipe, OrderPipe],
-	exports: [HumanizePipe, ThousandShortPipe, SafePipe, QualityPipe, TranslateStatusPipe, OrderPipe],
+	imports: [FormatPipeModule],
+	declarations: [HumanizePipe, ThousandShortPipe, SafePipe, QualityPipe, TranslateStatusPipe, OrderPipe, OmbiDatePipe],
+	exports: [HumanizePipe, ThousandShortPipe, SafePipe, QualityPipe, TranslateStatusPipe, OrderPipe, OmbiDatePipe],
+	providers: [FormatPipe],
 })
 export class PipeModule {
 	public static forRoot(): ModuleWithProviders<PipeModule> {
