f8c61027bf53d657d7955a98b69d7ab90b66a75a
David Benson <dben@users.noreply.github.com>
2024-07-11 04:40:43 -0400

    feat: Adding postgres support to ombi (beta) (#5050)

--- src/Ombi/Extensions/DatabaseExtensions.cs
+++ src/Ombi/Extensions/DatabaseExtensions.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using MySqlConnector;
 using Newtonsoft.Json;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal;
 using Ombi.Helpers;
 using Ombi.Store.Context;
 using Ombi.Store.Context.MySql;
 using Ombi.Store.Context.Sqlite;
+using Ombi.Store.Context.Postgres;
 using Polly;
 using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
 using SQLitePCL;
@@ -20,6 +24,7 @@ namespace Ombi.Extensions
 
         public const string SqliteDatabase = "Sqlite";
         public const string MySqlDatabase = "MySQL";
+        public const string PostgresDatabase = "Postgres";
 
         public static void ConfigureDatabases(this IServiceCollection services, IHealthChecksBuilder hcBuilder)
         {
@@ -36,6 +41,10 @@ namespace Ombi.Extensions
                     services.AddDbContext<OmbiContext, OmbiMySqlContext>(x => ConfigureMySql(x, configuration.OmbiDatabase));
                     AddMySqlHealthCheck(hcBuilder, "Ombi Database", configuration.OmbiDatabase);
                     break;
+                case var type when type.Equals(PostgresDatabase, StringComparison.InvariantCultureIgnoreCase):
+                    services.AddDbContext<OmbiContext, OmbiPostgresContext>(x => ConfigurePostgres(x, configuration.OmbiDatabase));
+                    AddPostgresHealthCheck(hcBuilder, "Ombi Database", configuration.OmbiDatabase);
+                    break;
             }
 
             switch (configuration.ExternalDatabase.Type)
@@ -48,6 +57,10 @@ namespace Ombi.Extensions
                     services.AddDbContext<ExternalContext, ExternalMySqlContext>(x => ConfigureMySql(x, configuration.ExternalDatabase));
                     AddMySqlHealthCheck(hcBuilder, "External Database", configuration.ExternalDatabase);
                     break;
+                case var type when type.Equals(PostgresDatabase, StringComparison.InvariantCultureIgnoreCase):
+                    services.AddDbContext<ExternalContext, ExternalPostgresContext>(x => ConfigurePostgres(x, configuration.ExternalDatabase));
+                    AddPostgresHealthCheck(hcBuilder, "External Database", configuration.ExternalDatabase);
+                    break;
             }
 
             switch (configuration.SettingsDatabase.Type)
@@ -59,6 +72,10 @@ namespace Ombi.Extensions
                 case var type when type.Equals(MySqlDatabase, StringComparison.InvariantCultureIgnoreCase):
                     services.AddDbContext<SettingsContext, SettingsMySqlContext>(x => ConfigureMySql(x, configuration.SettingsDatabase));
                     AddMySqlHealthCheck(hcBuilder, "Settings Database", configuration.SettingsDatabase);
+                    break;
+                case var type when type.Equals(PostgresDatabase, StringComparison.InvariantCultureIgnoreCase):
+                    services.AddDbContext<SettingsContext, SettingsPostgresContext>(x => ConfigurePostgres(x, configuration.SettingsDatabase));
+                    AddPostgresHealthCheck(hcBuilder, "Settings Database", configuration.SettingsDatabase);
                     break;
             }
         }
@@ -114,6 +131,19 @@ namespace Ombi.Extensions
             }
         }
 
+        private static void AddPostgresHealthCheck(IHealthChecksBuilder builder, string dbName, PerDatabaseConfiguration config)
+        {
+            if (builder != null)
+            {
+                builder.AddNpgSql(
+                    npgsqlConnectionString: config.ConnectionString,
+                    name: dbName,
+                    failureStatus: HealthStatus.Unhealthy,
+                    tags: new string[] { "db" }
+                    );
+            }
+        }
+
         public static void ConfigureSqlite(DbContextOptionsBuilder options, PerDatabaseConfiguration config)
         {
             SQLitePCL.Batteries.Init();
@@ -133,6 +163,14 @@ namespace Ombi.Extensions
                 //b.CharSetBehavior(Pomelo.EntityFrameworkCore.MySql.Infrastructure.CharSetBehavior.NeverAppend); // ##ISSUE, link to migrations?
                 b.EnableRetryOnFailure();
             });
+        }
+
+        public static void ConfigurePostgres(DbContextOptionsBuilder options, PerDatabaseConfiguration config)
+        {
+            options.UseNpgsql(config.ConnectionString, b =>
+            {
+                b.EnableRetryOnFailure();
+            }).ReplaceService<ISqlGenerationHelper, NpgsqlCaseInsensitiveSqlGenerationHelper>();
         }
 
         private static ServerVersion GetServerVersion(string connectionString)
@@ -190,6 +228,17 @@ namespace Ombi.Extensions
             }
             public string Type { get; set; }
             public string ConnectionString { get; set; }
+        }
+
+        public class NpgsqlCaseInsensitiveSqlGenerationHelper : NpgsqlSqlGenerationHelper
+        {
+            const string EFMigrationsHisory = "__EFMigrationsHistory";
+            public NpgsqlCaseInsensitiveSqlGenerationHelper(RelationalSqlGenerationHelperDependencies dependencies)
+                : base(dependencies) { }
+            public override string DelimitIdentifier(string identifier) =>
+                base.DelimitIdentifier(identifier == EFMigrationsHisory ? identifier : identifier.ToLower());
+            public override void DelimitIdentifier(StringBuilder builder, string identifier)
+                => base.DelimitIdentifier(builder, identifier == EFMigrationsHisory ? identifier : identifier.ToLower());
         }
     }
 }
