0fb29a0b16b1fc87f71df1a589f6141324cf2f1b
TidusJar <tidusjar@gmail.com>
2024-09-07 21:04:24 +0100

    fix(radarr-4k): :bug: Fixed an issue where the overrides wouldn't work for 4k Requests

--- src/Ombi/ClientApp/src/app/media-details/components/movie/panels/movie-advanced-options/movie-advanced-options.component.ts
+++ src/Ombi/ClientApp/src/app/media-details/components/movie/panels/movie-advanced-options/movie-advanced-options.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
-import { IAdvancedData, IRadarrProfile, IRadarrRootFolder } from "../../../../../interfaces";
+import { IAdvancedData, IRadarrProfile, IRadarrRootFolder, RequestCombination } from "../../../../../interfaces";
 import { RadarrService } from "../../../../../services";
 
 @Component({
@@ -11,6 +11,8 @@ export class MovieAdvancedOptionsComponent implements OnInit {
 
     public radarrProfiles: IRadarrProfile[];
     public radarrRootFolders: IRadarrRootFolder[];
+    public show4k: boolean = false;
+    public showNormal: boolean = false;
 
     constructor(public dialogRef: MatDialogRef<MovieAdvancedOptionsComponent>, @Inject(MAT_DIALOG_DATA) public data: IAdvancedData,
         private radarrService: RadarrService
@@ -19,16 +21,31 @@ export class MovieAdvancedOptionsComponent implements OnInit {
 
 
     public async ngOnInit() {
-        this.radarrService.getQualityProfilesFromSettings().subscribe(c => {
-            this.radarrProfiles = c;
-            this.data.profiles = c;
-            this.setQualityOverrides();
-        });
-        this.radarrService.getRootFoldersFromSettings().subscribe(c => {
-            this.radarrRootFolders = c;
-            this.data.rootFolders = c;
-            this.setRootFolderOverrides();
-        });
+        this.show4k = this.data.movieRequest.requestCombination === RequestCombination.FourK || this.data.movieRequest.requestCombination === RequestCombination.Both;
+        this.showNormal = this.data.movieRequest.requestCombination === RequestCombination.Normal || this.data.movieRequest.requestCombination === RequestCombination.Both;
+        if (this.show4k) {
+            this.radarrService.getQualityProfiles4kFromSettings().subscribe(c => {
+                this.radarrProfiles = c;
+                this.data.profiles4k = c;
+                this.setQualityOverrides();
+            });
+            this.radarrService.getRootFolders4kFromSettings().subscribe(c => {
+                this.radarrRootFolders = c;
+                this.data.rootFolders4k = c;
+                this.setRootFolderOverrides();
+            });
+        } else { // Currently show either 4k or normal, if it's a dual request there needs to be more work done to save the overrides for 4k separately
+            this.radarrService.getQualityProfilesFromSettings().subscribe(c => {
+                this.radarrProfiles = c;
+                this.data.profiles = c;
+                this.setQualityOverrides();
+            });
+            this.radarrService.getRootFoldersFromSettings().subscribe(c => {
+                this.radarrRootFolders = c;
+                this.data.rootFolders = c;
+                this.setRootFolderOverrides();
+            });
+        }
     }
 
     private setQualityOverrides(): void {
