635bed794e998c64cb9a0455399d083c50ac0ff6
Jamie Rees <Jamie.Rees@exclaimer.com>
2025-01-03 15:19:01 +0000

    feat(wizard): :sparkles: Added the ability to start with a different database

---
+++ src/Ombi/ClientApp/src/app/wizard/database/database.component.ts
@@ -0,0 +1,51 @@
+﻿import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { BehaviorSubject } from "rxjs";
+import { WizardService } from "../services/wizard.service";
+import { NotificationService } from "app/services";
+
+@Component({
+    templateUrl: "./database.component.html",
+    styleUrls: ["../welcome/welcome.component.scss"],
+    selector: "wizard-database-selector",
+})
+export class DatabaseComponent implements OnInit {
+    public constructor(private fb: FormBuilder, private service: WizardService, private notification: NotificationService) { }
+    @Output() public configuredDatabase = new EventEmitter<void>();
+
+    public form: FormGroup;
+
+    public connectionString = new BehaviorSubject<string>("Server=;Port=3306;Database=ombi");
+
+    public ngOnInit(): void {
+        this.form = this.fb.group({
+            type: ["MySQL"],
+            host: ["", [Validators.required]],
+            port: [3306, [Validators.required]],
+            name: ["ombi", [Validators.required]],
+            user: [""],
+            password: [""],
+        });
+
+        this.form.valueChanges.subscribe(x => {
+            let connection = `Server=${x.host};Port=${x.port};Database=${x.name}`;
+            if (x.user) {
+                connection = `Server=${x.host};Port=${x.port};Database=${x.name};User=${x.user}`;
+                if (x.password) {
+                    connection = `Server=${x.host};Port=${x.port};Database=${x.name};User=${x.user};Password=*******`;
+                }
+            }
+            this.connectionString.next(connection);
+        });
+    }
+
+    public save() {
+        this.service.addDatabaseConfig(this.form.value).subscribe(x => {
+            this.notification.success(`Database configuration updated! Please now restart ombi!`);
+            this.configuredDatabase.emit();
+        }, error => {
+            this.notification.error(error.error.message);
+        })
+    }
+
+}
--- src/Ombi/Controllers/V2/WizardController.cs
+++ src/Ombi/Controllers/V2/WizardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Ombi.Attributes;
 using Ombi.Core.Settings;
@@ -6,6 +8,10 @@ using Ombi.Helpers;
 using Ombi.Models.V2;
 using Ombi.Settings.Settings.Models;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MySqlConnector;
+using Npgsql;
+using Ombi.Core.Services;
 
 namespace Ombi.Controllers.V2
 {
@@ -13,19 +19,36 @@ namespace Ombi.Controllers.V2
     [AllowAnonymous]
     public class WizardController : V2Controller
     {
+        private readonly ISettingsService<OmbiSettings> _ombiSettings;
+        private readonly IDatabaseConfigurationService _databaseConfigurationService;
+        private readonly ILogger _logger;
         private ISettingsService<CustomizationSettings> _customizationSettings { get; }
 
-        public WizardController(ISettingsService<CustomizationSettings> customizationSettings)
+        public WizardController(
+            ISettingsService<CustomizationSettings> customizationSettings, 
+            ISettingsService<OmbiSettings> ombiSettings,
+            IDatabaseConfigurationService databaseConfigurationService,
+            ILogger<WizardController> logger)
         {
+            _ombiSettings = ombiSettings;
+            _databaseConfigurationService = databaseConfigurationService;
+            _logger = logger;
             _customizationSettings = customizationSettings;
         }
 
         [HttpPost("config")]
-        [ApiExplorerSettings(IgnoreApi =true)]
+        [ApiExplorerSettings(IgnoreApi = true)]
         public async Task<IActionResult> OmbiConfig([FromBody] OmbiConfigModel config)
         {
             if (config == null)
             {
+                return BadRequest();
+            }
+
+            var ombiSettings = await _ombiSettings.GetSettingsAsync();
+            if (ombiSettings.Wizard)
+            {
+                _logger.LogError("Wizard has already been completed");
                 return BadRequest();
             }
 
@@ -50,5 +73,66 @@ namespace Ombi.Controllers.V2
 
             return new OkObjectResult(settings);
         }
+
+        [HttpPost("database")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> DatabaseConfig([FromBody] WizardDatabaseConfiguration config, CancellationToken token)
+        {
+            if (config == null)
+            {
+                return BadRequest();
+            }            
+            
+            var ombiSettings = await _ombiSettings.GetSettingsAsync();
+            if (ombiSettings.Wizard)
+            {
+                _logger.LogError("Wizard has already been completed");
+                return BadRequest();
+            }
+
+            _logger.LogInformation("Setting up database type: {0}", config.Type);
+
+            var connectionString = string.Empty;
+            if (config.Type == IDatabaseConfigurationService.MySqlDatabase)
+            {
+                _logger.LogInformation("Building MySQL connectionstring");
+                var builder = new MySqlConnectionStringBuilder
+                {
+                    Database = config.Name,
+                    Port = Convert.ToUInt32(config.Port),
+                    Server = config.Host,
+                    UserID = config.User,
+                    Password = config.Password
+                };
+
+                connectionString = builder.ToString();
+            }
+
+            if (config.Type == IDatabaseConfigurationService.PostgresDatabase)
+            {
+                _logger.LogInformation("Building Postgres connectionstring");
+                var builder = new NpgsqlConnectionStringBuilder
+                {
+                    Host = config.Host,
+                    Port = config.Port,
+                    Database = config.Name,
+                    Username = config.User,
+                    Password = config.Password
+                };
+                connectionString = builder.ToString();
+            }
+
+            var result = await _databaseConfigurationService.ConfigureDatabase(config.Type, connectionString, token);
+            
+            if (!result)
+            {
+                return BadRequest(new DatabaseConfigurationResult(false, "Could not configure the database, please check the logs"));
+            }
+            
+            return Ok(new DatabaseConfigurationResult(true, "Database configured successfully"));
+        }
+
+        public record DatabaseConfigurationResult(bool Success, string Message);
+
     }
 }
