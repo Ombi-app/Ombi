ce5fae83553c96098f90178de29921163de44728
Michael Paulauski <mike10010100@gmail.com>
2025-10-06 19:22:02 -0400

    address sonarqube feedback

--- src/Ombi/ClientApp/src/app/settings/plex/components/watchlist/plex-watchlist.component.ts
+++ src/Ombi/ClientApp/src/app/settings/plex/components/watchlist/plex-watchlist.component.ts
@@ -110,7 +110,7 @@ export class PlexWatchlistComponent implements OnInit, OnDestroy{
     private createSnapshot(users: IPlexWatchlistUsers[]): string[] {
         return users
             .map(user => `${user.userId}:${user.syncStatus}`)
-            .sort();
+            .sort((a, b) => a.localeCompare(b));
     }
 
     private hasStateChanged(previous: string[], current: string[]): boolean {
